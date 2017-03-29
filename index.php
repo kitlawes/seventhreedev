c3e3a823cb8a36eb8a4a273b5b60cafde7765ffd
lrose07 <lrose07@vt.edu>
2017-03-29 09:39:28 -0400

    title updates

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
  <!DOCTYPE html>
 <html lang="en" class="js no-touch">
 <head>
-	<title>SevenThree Dev</title>
+	<title>SevenThree Dev | Small Business Software Solutions</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
